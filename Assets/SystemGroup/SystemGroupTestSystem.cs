e6cca6bff83bd1e3c798cad6e1547981777dc749
Rossfuin <darksheepgod@outlook.com>
2020-05-13 02:00:23 +0800

    add SystemGroup sample

---
+++ Assets/SystemGroup/SystemGroupTestSystem.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Scenes;
+
+namespace MyECSSample.SystemGroup
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [UpdateBefore(typeof(SceneSystem))]
+    public class SystemGroupTestSystem : ComponentSystem
+    {
+        protected override void OnUpdate() { }
+    }
+}
---
+++ Assets/SystemGroup/SystemGroupTestSystem2.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Scenes;
+
+namespace MyECSSample.SystemGroup
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [UpdateAfter(typeof(SystemGroupTestSystem))]
+    public class SystemGroupTestSystem2 : ComponentSystem
+    {
+        protected override void OnUpdate() { }
+    }
+}
